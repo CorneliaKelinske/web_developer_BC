82ba58318a5003860200ff00b981d939638f5def
CorneliaKelinske <corneliakelinske@gmail.com>
2020-10-09 11:13:56 -0700

    did my first tiny js exercise

---
+++ First Java Script Stuff/stalker_script.js
@@ -0,0 +1,5 @@
+var userFirst = prompt("What is your first name?");
+var userLast = prompt("What is your last name?");
+var userAge = prompt("What is your age");
+console.log("Hello, " + userFirst + " " + userLast);
+console.log("Your age is " + userAge)
